856c931bc8acbca9ad5658934faad9033410f201
outghter <oughter@eml.cc>
2017-03-28 21:37:19 -0400

    Add support for Meet

--- hook_hangouts.js
+++ hook_hangouts.js
@@ -1,21 +1,45 @@
+var isMeet = window.location.hostname === 'meet.google.com';
+
 var observer = new MutationObserver(function(mutations) {
-    var microphoneClass = 'IQ';
-    var cameraClass = 'OQ';
+    var elements = getElements();
+    var microphone = elements.microphone;
+    var camera = elements.camera;
 
-    var microphone = document.getElementsByClassName(microphoneClass);
-    var camera = document.getElementsByClassName(cameraClass);
-
-    if (microphone && camera && microphone[0] && camera[0]) {
-        hookHangouts(microphone[0], camera[0]);
+    if (microphone && camera) {
+        hookHangouts(microphone, camera);
     }
 });
 
 var observerConfig = {
+    attributes: true,
     childList: true
 };
 
 var targetNode = document.body;
 observer.observe(targetNode, observerConfig);
+
+function getElements() {
+    var camera;
+    var microphone;
+    var elements = {
+      camera: undefined,
+      microphone: undefined
+    };
+
+    if (isMeet) {
+      camera = document.querySelector('[data-capture-type="cam"]');
+      microphone = document.querySelector('[data-capture-type="mic"]');
+      elements.camera = camera && camera.querySelector('div[role="button"]');
+      elements.microphone = microphone && microphone.querySelector('div[role="button"]');
+    } else {
+      camera = document.getElementsByClassName('OQ');
+      microphone = document.getElementsByClassName('IQ');
+      elements.camera = camera && camera[0];
+      elements.microphone = microphone && microphone[0];
+    }
+
+    return elements;
+}
 
 function hookHangouts(microphone, camera) {
    chrome.storage.sync.get('muteMicrophone', function (result) {
@@ -31,10 +55,16 @@ function hookHangouts(microphone, camera) {
    });
 
    observer.disconnect();
-};
+}
 
 function simulateClick(item) {
-    item.dispatchEvent(new MouseEvent('mousedown'));
-    item.dispatchEvent(new MouseEvent('mouseup'));
-    item.dispatchEvent(new MouseEvent('mouseout'));
+    if (isMeet) {
+        setTimeout(function() {
+          item.click();
+        }, 100);
+    } else {
+      item.dispatchEvent(new MouseEvent('mousedown'));
+      item.dispatchEvent(new MouseEvent('mouseup'));
+      item.dispatchEvent(new MouseEvent('mouseout'));
+    }
 }
