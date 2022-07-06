d66294f46dc932c6a6e7bcef86fb7f18747de2d6
Joel <joel@deport.me>
2022-07-06 08:05:11 -0500

    Work with meet.google.com

--- hook_hangouts.js
+++ hook_hangouts.js
@@ -1,96 +1,41 @@
-function simulateClick(item) {
-  item.dispatchEvent(new PointerEvent('pointerdown', {bubbles: true}));
-  item.dispatchEvent(new MouseEvent('mousedown', {bubbles: true}));
-  item.dispatchEvent(new PointerEvent('pointerup', {bubbles: true}));
-  item.dispatchEvent(new MouseEvent('mouseup', {bubbles: true}));
-  item.dispatchEvent(new MouseEvent('mouseout', {bubbles: true}));
-  item.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+var observer = new MutationObserver(function(mutations) {
+  var elements = document.querySelectorAll('div[role=button]');
 
-  return true;
-}
+  var microphone = elements[0];
+  var camera = elements[1];
 
-function findValidElements(elements) {
-  for (var i=0; i < elements.length; i++) {
-    if (document.querySelector(elements[i]['microphone']['startElement'])) {
-      return elements[i]; 
-    }
-  }
-}
-
-function mute(muteMicrophone, muteCamera, validElements) {
-  // Try to mute items now
-  var mutedMicrophone = muteDevice('microphone', muteMicrophone, validElements);
-  var mutedCamera = muteDevice('camera', muteCamera, validElements);
-  
-  if (mutedMicrophone == false || mutedCamera == false) {
-    return false;
-  }
-
-  return true;
-}
-
-function muteDevice(device, mute, validElements) {
-  if (mute == true) {
-    var endElement = document.querySelector(validElements[device]['endElement']);
-    var startElement = document.querySelector(validElements[device]['startElement']);
-
-    if (endElement == null) {  
-      simulateClick(startElement);
-      return false;
-    }
-  }
-
-  return true;
-}
-
-var elements = [
-  {
-    microphone: 
-    {
-      'startElement': '.IQ',
-      'endElement': '.IQ[aria-pressed="true"]'
-    },
-    camera: 
-    {
-      'startElement': '.OQ',
-      'endElement': '.OQ[aria-pressed="true"]'
-    }
-  },
-  {
-    microphone: 
-    {
-      'startElement': '[data-capture-type="mic"] > div[role="button"]',
-      'endElement': '[data-capture-type="mic"] > .je'
-    },
-    camera: 
-    {
-      'startElement': '[data-capture-type="cam"] > div[role="button"]',
-      'endElement': '[data-capture-type="cam"] > .je'
-    }
-  }
-];
-
-chrome.storage.sync.get(['muteMicrophone', 'muteCamera'], function (result) {
-  // Only run if we want one of the inputs muted 
-  if (result.muteMicrophone || result.muteCamera) {
-    var elementsIntervalId;
-    var mutesIntervalId;
-
-    elementsIntervalId = setInterval(function() {
-      // We have a list of possible elements, let's find which we should be using
-      validElements = findValidElements(elements);
-
-      if (validElements) {
-        clearInterval(elementsIntervalId);
-        mutesIntervalId = setInterval(function() {
-          // Let's try muting devices now
-          muteSuccess = mute(result.muteMicrophone, result.muteCamera, validElements);
-
-          if (muteSuccess == true) {
-            clearInterval(mutesIntervalId);
-          }
-        }, 500);
-      }
-    }, 500);
+  if (microphone && camera) {
+    hookHangouts(microphone, camera);
   }
 });
+
+var observerConfig = {
+  childList: true
+};
+
+var targetNode = document.body;
+observer.observe(targetNode, observerConfig);
+
+function hookHangouts(microphone, camera) {
+  chrome.storage.sync.get('muteMicrophone', function (result) {
+    if (result.muteMicrophone && microphone.getAttribute('data-is-muted') != true) {
+      microphone.click();
+
+      // Initial modification of the element picked up by the mutation observer doesn't appear to be enough, don't stop observing until we're sure we've muted
+      if (microphone.getAttribute('data-is-muted') == true) {
+        observer.disconnect();
+      }
+    }
+  });
+
+  chrome.storage.sync.get('muteCamera', function (result) {
+    if (result.muteCamera & camera.getAttribute('data-is-muted') != true) {
+      camera.click();
+
+      // Initial modification of the element picked up by the mutation observer doesn't appear to be enough, don't stop observing until we're sure we've muted
+      if (camera.getAttribute('data-is-muted') == true) {
+        observer.disconnect();
+      }
+    }
+  });
+};
